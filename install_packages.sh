912685dd1360a7ec889bb1616e718ba043ae6a74
Martin Silha <martysilha@gmail.com>
2025-01-07 01:04:34 -0600

    feat: init

--- install_packages.sh
+++ install_packages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-section_title "$PINK" "Installing Required Packages"
+section_title "$YELLOW" "Installing Required Packages"
 
 console_message "$GREEN" "Installing required packages..."
 sudo apt update && sudo apt install -y feh lightdm xorg || {
     echo "Failed to install required packages."
+    console_message "$RED" "Failed to install required packages."
     exit 1
 }
--- main.sh
+++ main.sh
@@ -5,13 +5,20 @@ source "$(dirname "$0")/config.sh"
 trap 'echo "Error occurred. Exiting." && exit 1' ERR
 
 # Execute subscripts
+section_title "Creating User"
 # bash "$(dirname "$0")/create_user.sh"
+section_title "Install Packages"z
 # bash "$(dirname "$0")/install_packages.sh"
+section_title "Configuring System"
 # bash "$(dirname "$0")/config_lightdm.sh"
 # bash "$(dirname "$0")/config_firewall.sh"
+
+# Setup slideshow and health check
+section_title "CRON and Startup"
 bash "$(dirname "$0")/setup_slideshow.sh"
 bash "$(dirname "$0")/health_check.sh"
 
 # Reboot to apply changes
 echo "Setup complete! Rebooting..."
-sudo reboot
+console_message "$GREEN" "Setup complete! Rebooting..."
+# sudo reboot
