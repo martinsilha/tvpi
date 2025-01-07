4d77c9477fec6d0153eb32c16ff51c3a5c846b1f
Martin Silha <martysilha@gmail.com>
2025-01-07 17:09:55 -0600

    feat: update slideshow configuration to use dynamic user and directory paths

--- config.sh
+++ config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Configuration variables
-SLIDESHOW_USER="tvpitest"
-SLIDESHOW_DIR="/home/$SLIDESHOW_USER/slideshow/images"
+SLIDESHOW_USER=$(whoami)
+BASE_DIR="/home/$SLIDESHOW_USER"
+IMAGE_DIR="$BASE_DIR/Pictures"
+SLIDESHOW_DIR="$BASE_DIR/slideshow"
 SLIDESHOW_DELAY=120  # Delay in seconds for fbi
 SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
 SLACK_CHANNEL_ERROR="#error-all"
