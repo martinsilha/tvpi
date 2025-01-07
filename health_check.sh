85717eb9fdc0dc4f2b0786395272a52491739143
Martin Silha <martysilha@gmail.com>
2025-01-07 17:10:03 -0600

    feat: update health check script path and cron job configuration

--- health_check.sh
+++ health_check.sh
@@ -3,7 +3,7 @@ source "$(dirname "$0")/config.sh"
 
 section_title "Setup Health Check"
 console_info "Creating health check script..."
-sudo bash -c "cat <<EOF > /home/$SLIDESHOW_USER/health_check.sh
+sudo bash -c "cat <<EOF > $SLIDESHOW_DIR/$health_check.sh
 #!/bin/bash
 curl -X POST -H 'Content-type: application/json' --data \"{
     \\\"channel\\\": \\\"$SLACK_CHANNEL_ERROR\\\",
@@ -15,8 +15,8 @@ console_info "Setting permissions and adding to crontab..."
 sudo chmod +x /home/$SLIDESHOW_USER/health_check.sh
 
 # Check if the cron job already exists
-if ! sudo crontab -u $SLIDESHOW_USER -l 2>/dev/null | grep -q "/home/$SLIDESHOW_USER/health_check.sh"; then
-    (sudo crontab -u $SLIDESHOW_USER -l 2>/dev/null; echo "0 * * * * /home/$SLIDESHOW_USER/health_check.sh") | sudo crontab -u $SLIDESHOW_USER -
+if ! sudo crontab -u $SLIDESHOW_USER -l 2>/dev/null | grep -q "$SLIDESHOW_DIR/health_check.sh"; then
+    (sudo crontab -u $SLIDESHOW_USER -l 2>/dev/null; echo "0 * * * * $SLIDESHOW_DIR/health_check.sh") | sudo crontab -u $SLIDESHOW_USER -
     console_success "Health check cron job added."
 else
     console_info "Health check cron job already exists."
