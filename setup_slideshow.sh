73c5cb2f04bfa39f3e9d4d1c8689dd7fd3266426
Martin Silha <martysilha@gmail.com>
2025-01-07 17:10:18 -0600

    feat: enhance slideshow setup script with active console detection and path adjustments

--- setup_slideshow.sh
+++ setup_slideshow.sh
@@ -2,15 +2,24 @@
 source "$(dirname "$0")/config.sh"
 source "$(dirname "$0")/lib.sh"
 
+# Determine the active console
+ACTIVE_CONSOLE=$(fgconsole)
+
+# Verify if the console number is valid
+if [[ -z "$ACTIVE_CONSOLE" || "$ACTIVE_CONSOLE" -lt 1 ]]; then
+    echo "Error: Unable to determine the active console."
+    exit 1
+fi
+
 console_info "Setting up slideshow directory..."
 sudo mkdir -p "$SLIDESHOW_DIR"
-sudo chown -R $SLIDESHOW_USER:$SLIDESHOW_USER "/home/$SLIDESHOW_USER/slideshow"
+sudo chown -R $SLIDESHOW_USER:$SLIDESHOW_USER "$SLIDESHOW_DIR"
 
 console_info "Creating slideshow script..."
-sudo bash -c "cat <<EOF > /home/$SLIDESHOW_USER/start_slideshow.sh
+sudo bash -c "cat <<EOF > $SLIDESHOW_DIR/start_slideshow.sh
 #!/bin/bash
 while true; do
-    sudo fbi -T 1 -a -t $SLIDESHOW_DELAY -noverbose -random $SLIDESHOW_DIR/*
+    sudo fbi -T $ACTIVE_CONSOLE -a -t $SLIDESHOW_DELAY -noverbose -readahead $IMAGE_DIR/* --blend 60000
 done
 EOF"
-sudo chmod +x /home/$SLIDESHOW_USER/start_slideshow.sh
+sudo chmod +x $SLIDESHOW_DIR/start_slideshow.sh
