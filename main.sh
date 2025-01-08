8f89a77f48d10bfb38d4ba4733bdf1b54d9f7496
Martin Silha <martysilha@gmail.com>
2025-01-08 00:00:06 -0600

    fix: comment out directory creation and ownership adjustment in main.sh

--- main.sh
+++ main.sh
@@ -6,11 +6,6 @@ source "$(dirname "$0")/lib.sh"
 trap 'echo "Error occurred. Exiting." && exit 1' ERR
 
 # Execute subscripts
-# mkdir -p "$IMAGE_DIR"
-# mkdir -p "$SLIDESHOW_DIR"
-# chown -R $USER:$USER "$BASE_DIR"
-
-# bash "$(dirname "$0")/create_user.sh"  # Now only sets up directories
 bash "$(dirname "$0")/install_packages.sh"
 bash "$(dirname "$0")/config_firewall.sh"
 
