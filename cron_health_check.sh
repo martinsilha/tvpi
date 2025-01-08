3c1ab55e0f4e0b53495966bc00a76ef59b3ba9d8
Martin Silha <martysilha@gmail.com>
2025-01-07 20:17:24 -0600

    feat: add cron job for health check script with Slack notifications

---
+++ cron_health_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source "$(dirname "$0")/config.sh"
+
+section_title "Setup Health Check"
+console_info "Creating health check script..."
+sudo bash -c "cat <<EOF > $SLIDESHOW_DIR/health_check.sh
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data \"{
+    \\\"channel\\\": \\\"$SLACK_CHANNEL_ERROR\\\",
+    \\\"username\\\": \\\"$SLACK_USERNAME\\\",
+    \\\"text\\\": \\\"Health check: System is running.\\\"
+}\" $SLACK_WEBHOOK_URL
+EOF"
+console_info "Setting permissions and adding to crontab..."
+sudo chmod +x $SLIDESHOW_DIR/health_check.sh
+
+# Check if the cron job already exists
+if ! sudo crontab -u $SLIDESHOW_USER -l 2>/dev/null | grep -q "$SLIDESHOW_DIR/health_check.sh"; then
+    (sudo crontab -u $SLIDESHOW_USER -l 2>/dev/null; echo "0 * * * * $SLIDESHOW_DIR/health_check.sh") | sudo crontab -u $SLIDESHOW_USER -
+    console_success "Health check cron job added."
+else
+    console_info "Health check cron job already exists."
+fi
