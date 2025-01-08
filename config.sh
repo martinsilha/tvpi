74593443245acce5f0b1fee90c652aabe638cbac
Martin Silha <martysilha@gmail.com>
2025-01-07 20:52:31 -0600

    fix: remove Slack configuration variables from config script

--- config.sh
+++ config.sh
@@ -6,9 +6,6 @@ BASE_DIR="/home/$SLIDESHOW_USER"
 IMAGE_DIR="$BASE_DIR/Pictures"
 SLIDESHOW_DIR="$BASE_DIR/slideshow"
 SLIDESHOW_DELAY=120  # Delay in seconds for fbi
-SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
-SLACK_CHANNEL_ERROR="#error-all"
-SLACK_USERNAME="tvpi"
 
 # Source common functions
 source ./lib.sh
