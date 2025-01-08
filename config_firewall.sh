fbb5b6bd86eadcb0c5507e777ded99e4aaa11055
Martin Silha <martysilha@gmail.com>
2025-01-08 00:22:18 -0600

    fix: comment out firewall enabling command in config_firewall.sh

--- config_firewall.sh
+++ config_firewall.sh
@@ -10,6 +10,6 @@ console_info "Allowing outgoing traffic"
 sudo ufw default allow outgoing
 console_info "Allowing SSH"
 sudo ufw allow ssh
-console_info "Enabling firewall"
-yes | sudo ufw enable || console_warning "Firewall configuration skipped."
+# console_info "Enabling firewall"
+# yes | sudo ufw enable || console_warning "Firewall configuration skipped."
 console_success "Firewall installed and configured"
