3b80e54b617a36643a5f7fad13d47a550cdd4dc0
Martin Silha <martysilha@gmail.com>
2025-01-07 20:17:19 -0600

    fix: comment out incoming traffic denial in firewall configuration script

--- config_firewall.sh
+++ config_firewall.sh
@@ -4,8 +4,8 @@ source "$(dirname "$0")/config.sh"
 section_title "Firewall"
 console_info "Installing firewall"
 sudo apt install -y ufw
-console_info "Denying incoming traffic"
-sudo ufw default deny incoming
+# console_info "Denying incoming traffic"
+# sudo ufw default deny incoming
 console_info "Allowing outgoing traffic"
 sudo ufw default allow outgoing
 console_info "Allowing SSH"
