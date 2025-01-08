1abf1c6078796aff44f77c4e4e98490d1feace3a
Martin Silha <martysilha@gmail.com>
2025-01-08 00:00:14 -0600

    fix: update slideshow setup script to use specific user for ownership and streamline script creation

--- setup_slideshow.sh
+++ setup_slideshow.sh
@@ -11,13 +11,12 @@ fi
 
 console_info "Setting up slideshow directory..."
 mkdir -p "$SLIDESHOW_DIR"
-sudo chown -R $USER:$USER "$SLIDESHOW_DIR"
+chown -R $SLIDESHOW_USER:$SLIDESHOW_USER "$SLIDESHOW_DIR"
 
 console_info "Creating slideshow script..."
-bash -c "cat <<EOF > $SLIDESHOW_DIR/start_slideshow.sh
-#!/bin/bash
-while true; do
-    sudo fbi -d /dev/fb0 -T 1 -a -t $SLIDESHOW_DELAY --noverbose --readahead $IMAGE_DIR/*.{jpg,jpeg,webp} 2>/dev/null
-done
-EOF"
-chmod +x $SLIDESHOW_DIR/start_slideshow.sh
+create_script "$SLIDESHOW_DIR/start_slideshow.sh" "#!/bin/bash
+sudo fbi -d /dev/fb0 -T 1 -a -t $SLIDESHOW_DELAY --noverbose --readahead $IMAGE_DIR/*.{jpg,jpeg,webp} 2>/dev/null"
+
+console_info "Setting up cron job to restart slideshow script..."
+(crontab -l 2>/dev/null; echo "@reboot $SLIDESHOW_DIR/start_slideshow.sh") | crontab -
+console_success "Slideshow script and cron job set up successfully."
