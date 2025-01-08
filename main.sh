c161f09f543b87e8a80310cbf2b2a6cbaa0fd1a6
Martin Silha <martysilha@gmail.com>
2025-01-07 22:51:46 -0600

    fix: update main.sh to execute cron health check script instead of health check script

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ source "$(dirname "$0")/lib.sh"
 trap 'echo "Error occurred. Exiting." && exit 1' ERR
 
 # Execute subscripts
-bash "$(dirname "$0")/create_user.sh"
+bash "$(dirname "$0")/create_user.sh"  # Now only sets up directories
 bash "$(dirname "$0")/install_packages.sh"
 bash "$(dirname "$0")/config_firewall.sh"
 
@@ -15,7 +15,7 @@ section_title "CRON and Startup"
 console_info "Setup Slideshow"
 bash "$(dirname "$0")/setup_slideshow.sh"
 console_info "Setup Health Check"
-bash "$(dirname "$0")/health_check.sh"
+bash "$(dirname "$0")/cron_health_check.sh"
 
 # Reboot to apply changes
 console_success "Setup complete! Rebooting..."
