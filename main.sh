898107df073ffff7f88cd7503fc16a3fba954466
Martin Silha <martysilha@gmail.com>
2025-01-08 13:05:30 -0600

    fix: add health_check.sh creation with sudo in main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,11 @@ source "$(dirname "$0")/lib.sh"
 # Trap errors and handle them
 trap 'echo "Error occurred. Exiting." && exit 1' ERR
 
+# Create directories
+section_title "Creating Directories"
+sudo touch $SLIDESHOW_DIR/health_check.sh
+sudo chmod +x $SLIDESHOW_DIR/health_check.sh
+
 # Execute subscripts
 bash "$(dirname "$0")/install_packages.sh"
 # bash "$(dirname "$0")/config_firewall.sh"
