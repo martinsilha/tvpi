b4c27fe38519725de1a7fd22ed7c06141549f5b9
Martin Silha <martysilha@gmail.com>
2025-01-07 22:51:35 -0600

    fix: refactor user creation script to focus on directory setup

--- create_user.sh
+++ create_user.sh
@@ -2,20 +2,10 @@
 source "$(dirname "$0")/config.sh"
 source "$(dirname "$0")/lib.sh"
 
-section_title "$TITLE_COLOR" "Create Slideshow User"
-
-console_info "Creating slideshow linux user..."
-if id "$SLIDESHOW_USER" &>/dev/null; then
-    console_success "User $SLIDESHOW_USER already exists."
-else
-    sudo adduser --disabled-password --gecos "" $SLIDESHOW_USER || {
-        console_error "Failed to create user $SLIDESHOW_USER."
-        exit 1
-    }
-    sudo usermod -aG video,tty $SLIDESHOW_USER
-fi
+section_title "$TITLE_COLOR" "Setup Directories"
 
 console_info "Setting up directories..."
 sudo mkdir -p "$IMAGE_DIR"
 sudo mkdir -p "$SLIDESHOW_DIR"
 sudo chown -R $SLIDESHOW_USER:$SLIDESHOW_USER "$BASE_DIR"
+console_success "Directories set up successfully."
