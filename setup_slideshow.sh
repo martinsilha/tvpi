c48a20b6717420aec36b4689a6108b8190032854
Martin Silha <martysilha@gmail.com>
2025-01-08 22:13:52 -0600

    fix: simplify console availability check and update ExecStart command in setup_slideshow.sh

--- setup_slideshow.sh
+++ setup_slideshow.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-source "$(dirname "$0")/config.sh"
-source "$(dirname "$0")/lib.sh"
-
-# Verify if the console number is valid
-if [[ -e /dev/pts/0 ]]; then
-    echo "Console is available."
+echo "Console is available."
 else
     echo "Couldn't get a file descriptor referring to the console."
 fi
@@ -31,7 +25,7 @@ After=display-manager.service
 Requires=display-manager.service
 
 [Service]
-ExecStart=/bin/bash $SLIDESHOW_DIR/start_slideshow.sh
+ExecStart=/bin/bash sudo fbi -d /dev/fb0 -T 1 -a -t $SLIDESHOW_DELAY --noverbose --readahead $IMAGE_DIR/*.{jpg,jpeg,webp}
 Environment="DISPLAY=:0"
 Environment="XDG_RUNTIME_DIR=/run/user/1000"
 User=$SLIDESHOW_USER
@@ -45,8 +39,6 @@ TimeoutStopSec=0
 Restart=always
 RestartSec=10
 StartLimitBurst=5
-StartLimitIntervalSec=60
-
 
 [Install]
 WantedBy=graphical.target
@@ -55,9 +47,3 @@ EOF"
 console_info "Enabling and starting slideshow service..."
 sudo systemctl enable slideshow.service
 sudo systemctl start slideshow.service
-
-console_success "Slideshow service created and started successfully."
-
-# console_info "Setting up cron job to restart slideshow script..."
-# (crontab -l 2>/dev/null; echo "@reboot $SLIDESHOW_DIR/start_slideshow.sh") | crontab -
-# console_success "Slideshow script and cron job set up successfully."
